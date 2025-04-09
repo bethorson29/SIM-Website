ab123f3648221cf540fa7f32ee5130ad6ff1f422
bethorson29 <bethorson@gmail.com>
2025-04-09 09:32:23 -0600

    Add files via upload

---
+++ embed-calendar-v2.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://s3-us-west-2.amazonaws.com/assets.webconnex.com/widgets/calendar/widget.html?source=https://api.webconnex.com/v1/widgets/38cb0d13a36df09adffb5d0b6e3bc1c4?cache=no";
+  iframe.style.width = "100%";
+  iframe.style.height = "800px";
+  iframe.title = "widget";
+  iframe.frameBorder = "0";
+  document.getElementById("calendar-embed-container").appendChild(iframe);
+});
